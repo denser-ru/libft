af97e164e30ed3adf49b663bb395c533999edabe
Cheyenne Ayako <cayako@oa-i1.msk.21-school.ru>
2021-05-12 16:36:47 +0300

    v1.0.5_cwr fix test_ft_printf.c

--- src/tests/test_ft_printf.c
+++ src/tests/test_ft_printf.c
@@ -4,9 +4,10 @@
 int main()
 {
 	ft_putstr(ESC CLRSRC);
-	ft_frame((t_frame){1, 1, 30, 3, 0, 0});
-	ft_term_goto(2,2);
-	ft_printf("Hello, World!\n");
+	ft_frame((t_frame){1, 1, 24, 2, 0, 0});
+	ft_term_goto(5,2);
+	ft_printf(ESC "[%dm" "Привет, " ESC "[%dm" "Мир" ESC "[%dm" ESC "[0m"
+		"! =)\n", F_RED, F_BLUE, F_WHITE);
 	ft_term_goto(0,6);
 	return 0;
 }
