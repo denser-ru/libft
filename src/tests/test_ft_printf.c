b04d4fabccbc0f3a87b1dd04518dff75b0947cc8
Cheyenne Ayako <cayako@mi-v1.msk.21-school.ru>
2021-06-16 18:23:14 +0300

    v1.0.8_cwr fix test_ft_printf.c

--- src/tests/test_ft_printf.c
+++ src/tests/test_ft_printf.c
@@ -10,8 +10,20 @@ int main()
 //	ft_printf(ESC "[%dm" "Привет, " ESC "[%dm" "Мир" ESC "[%dm" ESC "[0m"
 //		"! =)\n", F_RED, F_BLUE, F_WHITE);
 //	ft_term_goto(0,4);
-	ft_printf("строка проверки: %05.f\n", 1);
-	printf("строка проверки: %05.f\n", 1);
+	ft_printf("my   строка проверки: %d\n", 1.01);
+	printf("orig строка проверки: %d\n", 1.01);
+	ft_putstr("\n");
+	ft_printf("строка проверки: %d\n", 1);
+	printf("строка проверки: %d\n", 1);
+	ft_putstr("\n");
+	ft_printf("строка проверки: %f\n", 1.01);
+	printf("строка проверки: %f\n", 1.01);
+	ft_putstr("\n");
+	ft_printf("строка проверки: %5f\n", 1.0);
+	printf("строка проверки: %5f\n", 1.0);
+	ft_putstr("\n");
+	ft_printf("строка проверки: %5.f\n", 1.0);
+	printf("строка проверки: %5.f\n", 1.0);
 //	ft_term_goto(0,6);
 	return 0;
 }
