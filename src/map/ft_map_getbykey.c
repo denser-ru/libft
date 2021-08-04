619a76a7275beee098644a13e8ca12fcc05633fc
OUT-Galkin-DS <mail@denser.ru>
2021-08-04 09:21:08 +0300

    v2.1.2 fix ft_map_getbykey.c

--- src/map/ft_map_getbykey.c
+++ src/map/ft_map_getbykey.c
@@ -8,7 +8,7 @@ t_list	*ft_map_getbykey(t_map *map, t_list *value, char *key)
 	elm = map->root;
 	while (elm)
 	{
-		if (!ft_strncmp(elm->key, key, elm->size_key))
+		if (!ft_strncmp(elm->key, key, elm->size_key + 1))
 		{
 			value->content = elm->value;
 			value->content_size = elm->size_value;
@@ -26,7 +26,7 @@ t_map_elm	*ft_map_get_elm(t_map *map, char *key)
 	elm = map->root;
 	while (elm)
 	{
-		if (!ft_strncmp(elm->key, key, elm->size_key))
+		if (!ft_strncmp(key, elm->key, elm->size_key + 1))
 			return (elm);
 		elm = elm->next;
 	}
